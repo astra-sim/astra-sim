210b2a4f3d763408b34ae24d3ba26a4243c0f806
System Administrator <root@ipsec-172-16-84-41.vpn.gatech.edu>
2020-07-14 03:49:32 -0400

    updated

---
+++ build.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+cd network
+if [ -d "gem5_astra" ]
+then
+	echo "You have already downloaded gem5"
+	cd ..
+	exit 1
+elif [ -d "ns3" ]
+then
+	echo "You have already downloaded ns3"
+	cd ..
+	exit 1
+fi
+
+read -p "Enter the back-end you want to download, options are gem5 or ns3: " backend
+
+if [ "$backend" == "gem5" ]
+then
+	git clone https://github.com/georgia-tech-synergy-lab/gem5_astra.git
+	echo "gem5 has been successfully downloaded"
+	cd .. 
+	exit 1
+elif [ "$backend" == "ns3" ]
+then
+	git clone 
+	echo "ns3 has been successfully downloaded"
+	cd ..
+	exit 1
+fi
