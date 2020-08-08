310aff2d70984905c26df3ae2154142b0cdc76ae
System Administrator <root@ipsec-172-16-87-121.vpn.gatech.edu>
2020-08-08 04:42:01 -0400

    updated

--- system/SimpleMemory.cc
+++ system/SimpleMemory.cc
@@ -23,7 +23,7 @@ uint64_t SimpleMemory::npu_mem_read(uint64_t size){
     }
     double offset=(max+delay)-time_ns;
     this->last_request_serviced=max+delay;
-    //std::cout<<"node id: "<<NI->sim_comm_get_rank()<<", access latency: "<<(uint64_t)(offset)
+    //std::cout<<"node id: "<<NI->sim_comm_get_rank()<<", bytes: "<<size<<", access latency: "<<(uint64_t)(offset)
              //<<", current delay: "<<delay<<", total requests: "<<request_count<<std::endl;
     return (uint64_t)(offset);
 
@@ -39,7 +39,7 @@ uint64_t SimpleMemory::SimpleMemory::npu_mem_write(uint64_t size){
     }
     double offset=(max+delay)-time_ns;
     this->last_request_serviced=max+delay;
-    //std::cout<<"node id: "<<NI->sim_comm_get_rank()<<", access latency: "<<(uint64_t)(offset)
+    //std::cout<<"node id: "<<NI->sim_comm_get_rank()<<", bytes: "<<size<<", access latency: "<<(uint64_t)(offset)
              //<<", current delay: "<<delay<<", total requests: "<<request_count<<std::endl;
     return (uint64_t)(offset);
 }
@@ -54,8 +54,8 @@ uint64_t SimpleMemory::nic_mem_read(uint64_t size){
     }
     double offset=(max+delay)-time_ns;
     this->last_request_serviced=max+delay;
-    //std::cout<<"node id: "<<NI->sim_comm_get_rank()<<", access latency: "<<(uint64_t)(offset)
-    //<<", current delay: "<<delay<<", total requests: "<<request_count<<std::endl;
+    //std::cout<<"node id: "<<NI->sim_comm_get_rank()<<", bytes: "<<size<<", access latency: "<<(uint64_t)(offset)
+             //<<", current delay: "<<delay<<", total requests: "<<request_count<<std::endl;
     return (uint64_t)(offset);
 }
 uint64_t SimpleMemory::nic_mem_write(uint64_t size){
@@ -69,7 +69,7 @@ uint64_t SimpleMemory::nic_mem_write(uint64_t size){
     }
     double offset=(max+delay)-time_ns;
     this->last_request_serviced=max+delay;
-    //std::cout<<"node id: "<<NI->sim_comm_get_rank()<<", access latency: "<<(uint64_t)(offset)
+    //std::cout<<"node id: "<<NI->sim_comm_get_rank()<<", bytes: "<<size<<", access latency: "<<(uint64_t)(offset)
              //<<", current delay: "<<delay<<", total requests: "<<request_count<<std::endl;
     return (uint64_t)(offset);
 }
