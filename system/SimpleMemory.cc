a209cd9da27f864e7a1f9cd1ef9f2a914ba65425
System Administrator <root@ipsec-172-16-87-121.vpn.gatech.edu>
2020-08-08 01:00:06 -0400

    updated

--- system/SimpleMemory.cc
+++ system/SimpleMemory.cc
@@ -1,76 +1,68 @@
-#include "AstraMemoryAPI.hh"
-#include "AstraNetworkAPI.hh"
+#include "SimpleMemory.hh"
 
-class SimpleMemory: public AstraMemoryAPI{
-public:
-    double last_request_serviced;
-    double npu_access_bw_GB;
-    double nic_access_bw_GB;
-    double access_latency;
-    AstraNetworkAPI *NI;
-    SimpleMemory(AstraNetworkAPI *NI,double access_latency,double npu_access_bw_GB,double nic_access_bw_GB){
+
+SimpleMemory::SimpleMemory(AstraNetworkAPI *NI,double access_latency,double npu_access_bw_GB,double nic_access_bw_GB){
         this->last_request_serviced=0;
         this->NI=NI;
         this->access_latency=access_latency;
         this->npu_access_bw_GB=npu_access_bw_GB;
         this->nic_access_bw_GB=nic_access_bw_GB;
+}
+void SimpleMemory::set_network_api(AstraNetworkAPI *astraNetworkApi){
+    this->NI=astraNetworkApi;
+}
+uint64_t SimpleMemory::npu_mem_read(uint64_t size){
+    timespec_t time=NI->sim_get_time();
+    double time_ns=time.time_val;
+    double delay=access_latency+(size/npu_access_bw_GB);
+    double max=last_request_serviced;
+    if(max<time_ns){
+        max=time_ns;
     }
-    void set_network_api(AstraNetworkAPI *astraNetworkApi){
-        this->NI=astraNetworkApi;
-    }
-    uint64_t npu_mem_read(uint64_t size){
-        timespec_t time=NI->sim_get_time();
-        double time_ns=time.time_val;
-        double delay=access_latency+(size/npu_access_bw_GB);
-        double max=last_request_serviced;
-        if(max<time_ns){
-            max=time_ns;
-        }
-        double offset=(max+delay)-time_ns;
-        this->last_request_serviced=max+delay;
-        return (uint64_t)(offset);
+    double offset=(max+delay)-time_ns;
+    this->last_request_serviced=max+delay;
+    return (uint64_t)(offset);
 
+}
+uint64_t SimpleMemory::SimpleMemory::npu_mem_write(uint64_t size){
+    timespec_t time=NI->sim_get_time();
+    double time_ns=time.time_val;
+    double delay=access_latency+(size/npu_access_bw_GB);
+    double max=last_request_serviced;
+    if(max<time_ns){
+        max=time_ns;
     }
-    uint64_t npu_mem_write(uint64_t size){
-        timespec_t time=NI->sim_get_time();
-        double time_ns=time.time_val;
-        double delay=access_latency+(size/npu_access_bw_GB);
-        double max=last_request_serviced;
-        if(max<time_ns){
-            max=time_ns;
-        }
-        double offset=(max+delay)-time_ns;
-        this->last_request_serviced=max+delay;
-        return (uint64_t)(offset);
+    double offset=(max+delay)-time_ns;
+    this->last_request_serviced=max+delay;
+    return (uint64_t)(offset);
+}
+uint64_t SimpleMemory::nic_mem_read(uint64_t size){
+    timespec_t time=NI->sim_get_time();
+    double time_ns=time.time_val;
+    double delay=access_latency+(size/nic_access_bw_GB);
+    double max=last_request_serviced;
+    if(max<time_ns){
+        max=time_ns;
     }
-    uint64_t nic_mem_read(uint64_t size){
-        timespec_t time=NI->sim_get_time();
-        double time_ns=time.time_val;
-        double delay=access_latency+(size/nic_access_bw_GB);
-        double max=last_request_serviced;
-        if(max<time_ns){
-            max=time_ns;
-        }
-        double offset=(max+delay)-time_ns;
-        this->last_request_serviced=max+delay;
-        return (uint64_t)(offset);
+    double offset=(max+delay)-time_ns;
+    this->last_request_serviced=max+delay;
+    return (uint64_t)(offset);
+}
+uint64_t SimpleMemory::nic_mem_write(uint64_t size){
+    timespec_t time=NI->sim_get_time();
+    double time_ns=time.time_val;
+    double delay=access_latency+(size/nic_access_bw_GB);
+    double max=last_request_serviced;
+    if(max<time_ns){
+        max=time_ns;
     }
-    uint64_t nic_mem_write(uint64_t size){
-        timespec_t time=NI->sim_get_time();
-        double time_ns=time.time_val;
-        double delay=access_latency+(size/nic_access_bw_GB);
-        double max=last_request_serviced;
-        if(max<time_ns){
-            max=time_ns;
-        }
-        double offset=(max+delay)-time_ns;
-        this->last_request_serviced=max+delay;
-        return (uint64_t)(offset);
-    }
-    uint64_t mem_read(uint64_t size){
-        return nic_mem_read(size);
-    }
-    uint64_t mem_write(uint64_t size){
-        return nic_mem_write(size);
-    }
-};
+    double offset=(max+delay)-time_ns;
+    this->last_request_serviced=max+delay;
+    return (uint64_t)(offset);
+}
+uint64_t SimpleMemory::mem_read(uint64_t size){
+    return nic_mem_read(size);
+}
+uint64_t SimpleMemory::mem_write(uint64_t size){
+    return nic_mem_write(size);
+}
---
+++ system/SimpleMemory.hh
@@ -0,0 +1,22 @@
+#ifndef __SIMPLEMEMORY_HH__
+#define __SIMPLEMEMORY_HH__
+#include "AstraMemoryAPI.hh"
+#include "AstraNetworkAPI.hh"
+
+class SimpleMemory: public AstraMemoryAPI{
+public:
+    double last_request_serviced;
+    double npu_access_bw_GB;
+    double nic_access_bw_GB;
+    double access_latency;
+    AstraNetworkAPI *NI;
+    SimpleMemory(AstraNetworkAPI *NI,double access_latency,double npu_access_bw_GB,double nic_access_bw_GB);
+    void set_network_api(AstraNetworkAPI *astraNetworkApi);
+    uint64_t npu_mem_read(uint64_t size);
+    uint64_t npu_mem_write(uint64_t size);
+    uint64_t nic_mem_read(uint64_t size);
+    uint64_t nic_mem_write(uint64_t size);
+    uint64_t mem_read(uint64_t size);
+    uint64_t mem_write(uint64_t size);
+};
+#endif
