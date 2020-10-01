0b9af96836acafaf974836a25364ec9e998be452
Saeed Rashidi <saeedrashidi@Saeeds-MacBook-Pro.local>
2020-10-01 00:07:21 -0400

    updated

---
+++ example/example.sh
@@ -0,0 +1,8 @@
+#! /bin/bash -v
+../AnalyticalBackend.sh -r \
+        --host-count=4 \
+        --total-stats=1 \
+        --stat-row=0 \
+        --path=../results/ \
+        --run-name=test \
+        --comm-scale=1 
