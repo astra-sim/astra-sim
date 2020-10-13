2be55976653911a949a6fe11160deb7180c5cb07
rashidi1saeed <saeed.rashidi@gatech.edu>
2020-10-13 00:18:40 -0400

    source file headers are updated

--- astra-sim/system/AstraComputeAPI.hh
+++ astra-sim/system/AstraComputeAPI.hh
@@ -1,3 +1,8 @@
+/******************************************************************************
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*******************************************************************************/
+
 #ifndef __COMPUTE_HH__
 #define __COMPUTE_HH__
 #include <cassert>
