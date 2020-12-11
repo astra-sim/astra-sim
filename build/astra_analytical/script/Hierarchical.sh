75e9f41996277c448a066ee96def5558ad49e500
Will Won <william.won@gatech.edu>
2020-12-11 04:38:22 -0500

    Add hierarchical script

--- build/astra_analytical/script/Hierarchical.sh
+++ build/astra_analytical/script/Hierarchical.sh
@@ -11,10 +11,10 @@ PROJECT_DIR="${SCRIPT_DIR:?}"/../../..
 INPUT_DIR=${PROJECT_DIR}/inputs
 COMPILE_SCRIPT="${SCRIPT_DIR:?}"/../build.sh
 BINARY="${SCRIPT_DIR:?}"/../build/AnalyticalAstra/bin/AnalyticalAstra
-NETWORK=${INPUT_DIR}/network/analytical/sample_Switch.json
-SYSTEM=${INPUT_DIR}/system/sample_a2a_sys
+NETWORK=${INPUT_DIR}/network/analytical/sample_Hierarchical.json
+SYSTEM=${INPUT_DIR}/system/sample_3dim_sys
 WORKLOAD=${INPUT_DIR}/workload/"$workload"
-STATS="${SCRIPT_DIR:?}"/../result/switch-${1:-result}
+STATS="${SCRIPT_DIR:?}"/../result/hierarchical-${1:-result}
 
 # create result directory
 rm -rf "${STATS}"
@@ -24,17 +24,18 @@ mkdir -p "${STATS}"
 echo "[SCRIPT] Compiling AnalyticalAstra"
 "${COMPILE_SCRIPT}" -c
 
-npus=(4 16 32 64 128 256 512 1024 2048)
+nodes=(2 4 8 16 32 64 128)
 commScale=(1)
 
 current_row=-1
-tot_stat_row=$((${#npus[@]} * ${#commScale[@]}))
+tot_stat_row=$((${#nodes[@]} * ${#commScale[@]}))
 
 # run test
-for npu in "${npus[@]}"; do
+for i in "${!nodes[@]}"; do
   for inj in "${commScale[@]}"; do
     current_row=$(($current_row + 1))
-    filename="workload-$workload-npus-${npu}-commScale-$inj"
+    totalNPUs=$((${nodes[$i]} * 16))
+    filename="workload-$workload-npus-$totalNPUs-commScale-$inj"
 
     echo "[SCRIPT] Initiate ${filename}"
 
@@ -43,16 +44,16 @@ for npu in "${npus[@]}"; do
       --system-configuration="${SYSTEM}" \
       --workload-configuration="${WORKLOAD}" \
       --path="${STATS}/" \
-      --units-count "${npu}" \
+      --units-count 2 8 "${nodes[$i]}" \
       --num-passes 2 \
       --num-queues-per-dim 1 \
-      --comm-scale ${inj} \
+      --comm-scale $inj \
       --compute-scale 1 \
       --injection-scale 1 \
       --rendezvous-protocol false \
-      --total-stat-rows "${tot_stat_row}" \
-      --stat-row "${current_row}" \
-      --run-name ${filename} >> "${STATS}/${filename}.txt" &
+      --total-stat-rows "$tot_stat_row" \
+      --stat-row "$current_row" \
+      --run-name $filename >> "${STATS}/$filename.txt" &
       
       sleep 1
   done
--- build/astra_analytical/script/Torus2D.sh
+++ build/astra_analytical/script/Torus2D.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 workload=medium_DLRM
 
 # Absolue path to this script
