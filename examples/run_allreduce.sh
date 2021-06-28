f2c54cbbc0a749b6fc7aadc20b97d834d5011411
rashidi1saeed <saeed.rashidi@gatech.edu>
2021-06-27 20:34:15 -0400

    inputs updated

--- examples/run_allreduce.sh
+++ examples/run_allreduce.sh
@@ -30,7 +30,7 @@ then
 elif [ "$network" == "analytical" ]
 then
     BINARY="${SCRIPT_DIR:?}"/../build/astra_analytical/build/AnalyticalAstra/bin/AnalyticalAstra
-	NETWORK="${SCRIPT_DIR:?}"/../inputs/network/analytical/sample_Torus2D.json
+	NETWORK="${SCRIPT_DIR:?}"/../inputs/network/analytical/sample_Torus3D.json
 fi
 
 rm -rf "${STATS}"
--- examples/run_multi.sh
+++ examples/run_multi.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(dirname "$(realpath $0)")
 
 # Absolute paths to useful directories
 BINARY="${SCRIPT_DIR:?}"/../build/astra_analytical/build/AnalyticalAstra/bin/AnalyticalAstra
-NETWORK="${SCRIPT_DIR:?}"/../inputs/network/analytical/sample_Torus2D.json
+NETWORK="${SCRIPT_DIR:?}"/../inputs/network/analytical/sample_Torus3D.json
 SYSTEM="${SCRIPT_DIR:?}"/../inputs/system/sample_torus_sys.txt
 WORKLOAD="${SCRIPT_DIR:?}"/../inputs/workload/microAllReduce.txt
 STATS="${SCRIPT_DIR:?}"/results/run_multi
@@ -22,14 +22,14 @@ tot_stat_row=`expr ${#npus[@]} \* ${#commScale[@]}`
 for i in "${!npus[@]}"; do
   for inj in "${commScale[@]}"; do
     current_row=`expr $current_row + 1`
-    tot_npus=`expr ${npus[$i]} \* ${npus[$i]} `
+    tot_npus=`expr ${npus[$i]} \* ${npus[$i]} \* ${npus[$i]} `
     filename="npus-$tot_npus-commScale-$inj"
 	"${BINARY}" \
     --network-configuration="${NETWORK}" \
     --system-configuration="${SYSTEM}" \
     --workload-configuration="${WORKLOAD}" \
     --path="${STATS}/" \
-    --units-count "${npus[$i]}" "${npus[$i]}" \
+    --units-count "${npus[$i]}" "${npus[$i]}" "${npus[$i]}" \
     --links-count 2 2 \
     --num-passes 2 \
     --num-queues-per-dim 1 \
