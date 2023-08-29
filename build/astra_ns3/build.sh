1f74a7e21d28983bceb7ba970c58784c1e847450
jinsun-yoo <38969914+jinsun-yoo@users.noreply.github.com>
2023-08-29 14:26:32 -0400

    Update build.sh to use gcc 4.9 instead of 5
    Following PR #96

--- build/astra_ns3/build.sh
+++ build/astra_ns3/build.sh
@@ -34,7 +34,7 @@ function compile {
     cp "${ASTRA_SIM_DIR}"/network_frontend/ns3/*.h "${NS3_DIR}"/simulation/scratch/
     cp "${ASTRA_SIM_DIR}"/network_frontend/ns3/wscript "${NS3_DIR}"/simulation/src/applications
     cd "${NS3_DIR}/simulation"
-    CC='gcc-5' CXX='g++-5' ./waf configure 
+    CC='gcc-4.9' CXX='g++-4.9' ./waf configure 
     ./waf --run 'scratch/AstraSimNetwork mix/config.txt --commscale=1'
     cd "${SCRIPT_DIR:?}"
 }
