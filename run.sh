c90d36c787da0933036d8bc5e241d20dca9be3be
Saeed Rashidi <rashidi1saeid@gmail.com>
2023-10-06 00:25:45 -0700

    minor modifications made

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(dirname "$(realpath $0)")
 BINARY="${SCRIPT_DIR:?}"/build/astra_analytical/build/AnalyticalAstra/bin/AnalyticalAstra
-WORKLOAD="${SCRIPT_DIR:?}"/extern/graph_frontend/chakra/eg_files/dlrm_chakra
+WORKLOAD="${SCRIPT_DIR:?}"/extern/graph_frontend/chakra/et_plus/dlrm_chakra
 SYSTEM="${SCRIPT_DIR:?}"/inputs/system/sample_fully_connected_sys.txt
 NETWORK="${SCRIPT_DIR:?}"/inputs/network/analytical/fully_connected.json
 MEMORY="${SCRIPT_DIR:?}"/inputs/memory/analytical/no_memory_expansion.json
