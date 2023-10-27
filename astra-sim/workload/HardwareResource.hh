d8ef01a045753f5e9f8df2c364d892d1530c3507
Taekyung Heo <7621438+TaekyungHeo@users.noreply.github.com>
2023-10-27 07:37:59 -0400

    Add comments to HardwareResource.*

--- astra-sim/workload/HardwareResource.hh
+++ astra-sim/workload/HardwareResource.hh
@@ -3,6 +3,8 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 *******************************************************************************/
 
+// TODO: HardwareResource.hh should be moved to the system layer.
+
 #ifndef __HARDWARE_RESOURCE_HH__
 #define __HARDWARE_RESOURCE_HH__
 
