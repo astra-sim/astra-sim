41b4f166b798eb872f20a3a90804d3b1c4be89df
Divya Kiran Kadiyala <dkadiyala3@gatech.edu>
2023-10-31 04:14:24 -0400

    Added README to checkout correct branch and added the covnverter script

--- inputs/workload/conv-text-to-exegraph.sh
+++ inputs/workload/conv-text-to-exegraph.sh
@@ -6,10 +6,10 @@
 #WORKLOAD="Transformer_HybridParallel_Fwd_In_Bckwd"
 WORKLOAD="Resnet50_DataParallel"
 
-PYTHONPATH=$(pwd)/chakra python3 -m et_converter.et_converter\
+PYTHONPATH=$(pwd)/../../extern/graph_frontend/chakra python3 -m et_converter.et_converter\
  --input_type Text\
- --input_filename ../../inputs/workload/ASTRA-sim-1.0/${WORKLOAD}.txt\
- --output_filename ../../inputs/workload/ASTRA-sim-2.0/${WORKLOAD}\
+ --input_filename ./ASTRA-sim-1.0/${WORKLOAD}.txt\
+ --output_filename ./ASTRA-sim-2.0/${WORKLOAD}\
  --num_npus 64\
  --num_dims 1\
  --num_passes 1
