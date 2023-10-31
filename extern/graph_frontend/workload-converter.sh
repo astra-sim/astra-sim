138d2ecd1682f6751894eb2ff36285e4f3b9b65c
Divya Kiran Kadiyala <dkadiyala3@gatech.edu>
2023-10-31 03:16:49 -0400

    fixed bug in analytical/congestion_unaware/main.cc and added workload-converter.sh in inputs/workload/

---
+++ extern/graph_frontend/workload-converter.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#WORKLOAD="microAllReduce"
+#WORKLOAD="microAllToAll"
+#WORKLOAD="MLP_HybridParallel_Data_Model"
+#WORKLOAD="Transformer_HybridParallel"
+#WORKLOAD="Transformer_HybridParallel_Fwd_In_Bckwd"
+WORKLOAD="Resnet50_DataParallel"
+
+PYTHONPATH=$(pwd)/chakra python3 -m et_converter.et_converter\
+ --input_type Text\
+ --input_filename ../../inputs/workload/ASTRA-sim-1.0/${WORKLOAD}.txt\
+ --output_filename ../../inputs/workload/ASTRA-sim-2.0/${WORKLOAD}\
+ --num_npus 64\
+ --num_dims 1\
+ --num_passes 1
