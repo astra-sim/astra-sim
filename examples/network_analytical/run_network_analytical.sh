ebdbe53f46543cd858867fd1640b88feb293003b
Will Won <william.won@gatech.edu>
2024-11-02 17:34:51 -0400

    Add example run script

---
+++ examples/network_analytical/run_network_analytical.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+## ******************************************************************************
+## This source code is licensed under the MIT license found in the
+## LICENSE file in the root directory of this source tree.
+##
+## Copyright (c) 2024 Georgia Institute of Technology
+## ******************************************************************************
+
+# find the absolute path to this script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+PROJECT_DIR="${SCRIPT_DIR:?}/../.."
+
+# paths
+ASTRA_SIM="${PROJECT_DIR:?}/build/astra_analytical/build/bin/AstraSim_Analytical_Congestion_Aware"
+
+# start
+echo "[ASTRA-sim] Compiling ASTRA-sim with the Analytical Network Backend..."
+echo ""
+
+# Compile
+"${PROJECT_DIR:?}"/build/astra_analytical/build.sh
+
+echo ""
+echo "[ASTRA-sim] Compilation finished."
+echo "[ASTRA-sim] Running ASTRA-sim Example with Analytical Network Backend..."
+echo ""
+
+# run ASTRA-sim
+"${ASTRA_SIM:?}"
+
+# finalize
+echo ""
+echo "[ASTRA-sim] Finished the execution."
