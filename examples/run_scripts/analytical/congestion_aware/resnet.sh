bd3333b711392da8720a289043146dd4d585e51b
Changhai Man <changhai0109@hotmail.com>
2025-10-27 15:54:57 -0400

    add mixtral experiment 	new file:   examples/run_scripts/analytical/congestion_aware/mixtral_8x22b.sh 	new file:   examples/run_scripts/analytical/congestion_aware/mixtral_8x7b.sh 	new file:   examples/run_scripts/analytical/congestion_aware/resnet.sh 	modified:   inputs/system/analytical/hgx_h100_32gpu.json 	modified:   inputs/system/analytical/hgx_h100_8gpu.json

---
+++ examples/run_scripts/analytical/congestion_aware/resnet.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+## ******************************************************************************
+## This source code is licensed under the MIT license found in the
+## LICENSE file in the root directory of this source tree.
+##
+## Copyright (c) 2024 Georgia Institute of Technology
+## ******************************************************************************
+
+# find the absolute path to this script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+PROJECT_DIR="${SCRIPT_DIR:?}/../../../.."
+EXAMPLE_DIR="${PROJECT_DIR:?}/examples"
+
+# paths
+ASTRA_SIM="${PROJECT_DIR:?}/build/astra_analytical/build/bin/AstraSim_Analytical_Congestion_Aware"
+WORKLOAD="${EXAMPLE_DIR:?}/../runs/result/merged"
+SYSTEM="${EXAMPLE_DIR:?}/system/native_collectives/HGX-H100-validated.json"
+NETWORK="${EXAMPLE_DIR:?}/network/analytical/HGX-H100-validated.yml"
+REMOTE_MEMORY="${EXAMPLE_DIR:?}/remote_memory/analytical/no_memory_expansion.json"
+
+# start
+echo "[ASTRA-sim] Compiling ASTRA-sim with the Analytical Network Backend..."
+echo ""
+
+# Compile
+"${PROJECT_DIR:?}"/build/astra_analytical/build.sh
+
+echo ""
+echo "[ASTRA-sim] Compilation finished."
+echo "[ASTRA-sim] Running ASTRA-sim Example with Analytical Network Backend..."
+echo ""
+
+# run ASTRA-sim
+"${ASTRA_SIM:?}" \
+    --workload-configuration="${WORKLOAD}" \
+    --system-configuration="${SYSTEM:?}" \
+    --remote-memory-configuration="${REMOTE_MEMORY:?}" \
+    --network-configuration="${NETWORK:?}"
+
+# finalize
+echo ""
+echo "[ASTRA-sim] Finished the execution."
